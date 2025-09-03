fd0eab18003aa3a4988c733c379fd4038aac9921
KAMILA NERE DE OLIVEIRA <kamilan.oliveira27@gmail.com>
2025-09-03 08:19:03 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 // src/main.tsx
+import "./theme.css";   // <- usar o arquivo que existe
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
